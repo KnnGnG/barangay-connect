f3bdcee8aa50b87d1bef499618829b5242131ae4
Kenn <kenn.guk-ong.22@usjr.edu.ph>
2025-06-20 04:55:20 +0800

    Feedback pages

--- src/app/pages/feedback/feedback.module.ts
+++ src/app/pages/feedback/feedback.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FeedbackPageRoutingModule } from './feedback-routing.module';
-
 import { FeedbackPage } from './feedback.page';
+import { StarRatingComponent } from '../../components/star-rating/star-rating.component'; // Add this import
 
 @NgModule({
   imports: [
@@ -15,6 +13,6 @@ import { FeedbackPage } from './feedback.page';
     IonicModule,
     FeedbackPageRoutingModule
   ],
-  declarations: [FeedbackPage]
+  declarations: [FeedbackPage, StarRatingComponent] // Add StarRatingComponent here
 })
 export class FeedbackPageModule {}
--- src/app/pages/feedback/feedback.page.ts
+++ src/app/pages/feedback/feedback.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-feedback',
@@ -6,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.page.scss'],
   standalone: false,
 })
-export class FeedbackPage implements OnInit {
+export class FeedbackPage {
+  serviceType: string = '';
+  rating: number = 0;
+  comments: string = '';
+  
+  feedbackHistory = [
+    { serviceType: 'document', rating: 4, comments: 'Fast processing but staff could be more friendly', date: new Date('2023-05-20') },
+    { serviceType: 'complaint', rating: 5, comments: 'My complaint was resolved quickly', date: new Date('2023-06-02') }
+  ];
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  submitFeedback() {
+    if (this.serviceType && this.rating > 0) {
+      const newFeedback = {
+        serviceType: this.serviceType,
+        rating: this.rating,
+        comments: this.comments,
+        date: new Date()
+      };
+      this.feedbackHistory.push(newFeedback);
+      console.log('Submitted feedback:', newFeedback);
+      this.resetForm();
+    }
   }
 
+  getServiceTypeName(type: string): string {
+    const types: {[key: string]: string} = {
+      document: 'Document Processing',
+      complaint: 'Complaint Handling',
+      appointment: 'Appointment',
+      general: 'General Service'
+    };
+    return types[type] || type;
+  }
+
+  resetForm() {
+    this.serviceType = '';
+    this.rating = 0;
+    this.comments = '';
+  }
 }
