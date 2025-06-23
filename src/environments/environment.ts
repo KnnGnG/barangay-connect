b419175bdc03f4d286c271f7869b5ec8a8c7bfb7
Kenn <kenn.guk-ong.22@usjr.edu.ph>
2025-06-23 23:40:40 +0800

    firebaseconfig & envireonment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyC2WP_REo5GigB0KHGr7U6ST8vIXk5J7cc",
+    authDomain: "barangay-connect-ddd96.firebaseapp.com",
+    projectId: "barangay-connect-ddd96",
+    storageBucket: "barangay-connect-ddd96.appspot.com",
+    messagingSenderId: "406739313373",
+    appId: "1:406739313373:web:76742eac0ce4a5f1f6f134"
+  }
 };
 
 /*
